ba8de54269d45a72156b19fc985fb06047755b6c
Arslan Memon <muhammadmemon@Muhammads-MBP.fios-router.home>
2015-11-21 22:13:02 -0600

    Initial Commit

---
+++ project4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project4
+//
+//  Created by Arslan Memon on 11/21/15.
+//  Copyright (c) 2015 Arslan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
